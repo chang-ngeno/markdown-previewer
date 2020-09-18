0e45bd066c3c4ad05f2ea924026af7ce7606027d
Daniel C <developer@macoz.co.ke>
2020-09-19 01:57:37 +0300

    Updates

---
+++ precache-manifest.0777701da5969ab821cd45f06d6d204b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0d5ff41ef78657f8db1bd05d8ccebb",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "d9ec2b1446cea2cc0fa6",
+    "url": "/markdown-previewer/static/css/main.b5cfb381.chunk.css"
+  },
+  {
+    "revision": "64c4a04ad4072d67f3eb",
+    "url": "/markdown-previewer/static/js/2.762c06a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.762c06a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9ec2b1446cea2cc0fa6",
+    "url": "/markdown-previewer/static/js/main.27e36abe.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
